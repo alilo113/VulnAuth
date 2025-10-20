b229eb7e1490978be021b06b41697c55ff84287d
alilo113 <aliopdandan@gmail.com>
2025-10-20 11:14:47 +0100

    f

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ import express from "express"
 
 const app = express()
 
-app.get("/", (req,res) => {
-	res.json({message: "Hello world"})
+app.use(express.json())
+
+let userDB = []
+app.post("/signup", async (req, res) => {
+    try {
+        const {username, password, email} = req.body
+        userDB.push({
+            username: username,
+            password: password,
+            eamil: email
+        })
+        console.log(userDB)s
+        res.status(200).send.json({message: "User signed up successfully"})
+    } catch (error) {
+        res.status(500).json({message: "Internal server error"})
+        console.log(error)
+    }
 })
 
-app.listen(() => {
-	console.log("This app listen to port 3000") 
+app.listen(3000, () => {
+    console.log("This app is listening to port 3000")
 })
